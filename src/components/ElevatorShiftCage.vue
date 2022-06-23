113ea6e5acbef233eba3cd32eb25e52093f4bc56
SlavicC1 <v.shinckaruck@yandex.ru>
2022-06-23 15:48:06 +0300

    elevator indication

--- src/components/ElevatorShiftCage.vue
+++ src/components/ElevatorShiftCage.vue
@@ -2,6 +2,12 @@
   <div 
     :class="cageClass" 
     :style="{ bottom: cageState.bottom + 'px' }">
+      <div v-if="cageState.state === 'moving'"
+        class="cage-indication">
+        <div v-if="cageState.direction === 'up'">&uArr;</div>
+        <div v-if="cageState.direction === 'down'">&dArr;</div>
+        {{cageState.floor}}
+      </div>
   </div>
 </template>
 
@@ -42,5 +48,13 @@ export default {
 
   .waiting {
     background: red;
+  }
+
+  .cage-indication {
+    width: 20px;
+    text-align: center;
+    margin: 10px auto;
+    border: 1px solid black;
+    background: white;
   }
 </style>
